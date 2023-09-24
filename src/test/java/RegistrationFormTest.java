6b33b274b19871a6257384fa30518cc5f8b4b4a7
Yury Vitovtov <vitovtov.y@gmail.com>
2023-09-25 00:06:58 +0700

    Добавил поля

--- src/test/java/RegistrationFormTest.java
+++ src/test/java/RegistrationFormTest.java
@@ -19,24 +19,29 @@ public class RegistrationFormTest {
     static void beforeAll() {
         Configuration.baseUrl = "https://app.qa.guru";
         Configuration.browserSize = "1920x1080";
-        //Configuration.holdBrowserOpen = true;
+        Configuration.holdBrowserOpen = true;
     }
 
     @Test
     void successfulRegistrationTest() {
+        String firstName = "Yury";
         open("/automation-practice-form");
-      //  $(".MuiTypography-h4").shouldHave(text("Practice form"));
+        $(".MuiTypography-h4").shouldHave(text("Practice form"));
 
 
-        $("#\\:r0\\:").setValue("Yury");
+        $("#\\:r0\\:").setValue(firstName);
         $("#\\:r2\\:").setValue("vitovtov.y@test.com");
         $("#\\:r1\\:").setValue("Vitovtov");
         $("#\\:r3\\:").setValue("2345678901");
         $("#\\:r7\\:").setValue("2755 Broadway");
+        $(byText("Language")).parent().click();
+        $(byText("Russian")).click();
+
 
         $("[value=Male]").click();
         $("[value=Sports]").click();
         $("[value=Reading]").click();
+        $("#\\:r4\\:").setValue("06/11/1991");
         $(byText("Subjects")).parent().click();
         $(byText("Science")).click();
         $(byText("Maths")).click();
@@ -51,8 +56,9 @@ public class RegistrationFormTest {
 
 
         $(".css-105y4gd").shouldHave(text("Yury"), text("vitovtov.y@test.com"), text("Vitovtov"),
-                text("+1 234 567 8901"), text("Science"), text("2755 Broadway"), text("Male"),
-                text("Sports"), text("Reading"), text("Science"), text("Maths"), text("Florida"), text("Tampa"));
-
+                text("+1 234 567 8901"), text("2755 Broadway"), text("Male"),
+                text("Sports"), text("Reading"), text("Science"), text("Maths"), text("Florida"),
+                text("Russian"), text("Tampa"));
+        $(byText("Language")).click();
     }
 }
